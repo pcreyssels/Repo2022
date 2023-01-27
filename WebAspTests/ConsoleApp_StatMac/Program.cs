6a04828b272653c98a4c29ffeb05ffeac524771e
pcreyssels <pcreyssels@free.fr>
2023-01-27 17:32:12 +0100

    a

--- WebAspTests/ConsoleApp_StatMac/Program.cs
+++ WebAspTests/ConsoleApp_StatMac/Program.cs
@@ -1,24 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ConsoleApp_StatMac;
+
 Console.WriteLine("Hello, World!");
-Dictionary<SMState, string> SMStateDescription=null;
-SMStateDescription = new Dictionary<SMState, string>();
-//SMStateDescription.Add(new SMState(, "");
 
-var SMState_values = Enum.GetValues<SMState>();
+StateMachine sma = new StateMachine(null);
+// définition des labels états
+sma.setSMStateDescription(StateMachine.SMState.state_1, "etat 1");
+sma.setSMStateDescription(StateMachine.SMState.state_2, "etat 2");
+sma.setSMStateDescription(StateMachine.SMState.state_2, "etat 3");
 
-for (int j = 0;j< SMState_values.Length;j++)
-{
-    SMStateDescription.Add(SMState_values[j], "");
-}
+sma.currentState = StateMachine.SMState.state_0;
+// définition des variables externes
+sma.ExtVarDef.Add("param1_int",typeof(int));
+sma.ExtVarDef.Add("param2_string", typeof(string));
+sma.ExtVarDef.Add("param3_int", typeof(int));
+sma.ExtVarDef.Add("param4_string", typeof(int));
+sma.ExtVarDef.Add("param5_BOOL", typeof(bool));
+
+// définition des valeurs des variables externes
+sma.ExtVarVal.Add("param1_int", 5);
+sma.ExtVarVal.Add("param2_string", "bonjour");
+sma.ExtVarVal.Add("param3_int", 14);
+sma.ExtVarVal.Add("param4_string", "chaine 5");
+sma.ExtVarVal.Add("param5_BOOL", true);
+
+
+
+//***************
+
+//Dictionary<SMState0, string> SMStateDescription=null;
+//SMStateDescription = new Dictionary<SMState0, string>();
+////SMStateDescription.Add(new SMState(, "");
+
+//var SMState_items = Enum.GetValues<SMState0>();
+
+//for (int j = 0;j< SMState_items.Length;j++)
+//{
+//    SMStateDescription.Add(SMState_items[j], "");
+//}
 
 int z = 3;
 
-SMStateDescription[SMState.state_8] = "bonjour";
+//SMStateDescription[SMState0.state_8] = "bonjour";
 
 z= 4;
 
-public enum SMState : ushort
+public enum SMState0 : ushort
 {
     state_0 = 0,
     state_1 = 1,
--- WebAspTests/ConsoleApp_StatMac/StateMachine.cs
+++ WebAspTests/ConsoleApp_StatMac/StateMachine.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleApp_StatMac
 {
 
-    
+    using _ExtVarDef = Dictionary<string, Type>;
+    using _ExtVarVal = Dictionary<string, object>;
 
     internal class StateMachine
     {
@@ -26,35 +28,16 @@ namespace ConsoleApp_StatMac
         }
         
         private SMState _currentState;
-
-        private Dictionary<SMState, string> SMStateDescription;
-
-        private Dictionary<SMState, List<SMState>> SMStatesTransitions;
-
         public SMState currentState
         {
             get => currentState;
-
-            set
-            {
-                if (true)
-                {
+            set {
                     _currentState = value;
-                }
             }
-        
         }
 
-        public StateMachine()
-        {
-            SMStateDescription = new Dictionary<SMState, string>();
-            
-            var SMState_values = Enum.GetValues<SMState>();
-            for (int i = 0; i < SMState_values.Length; i++)
-            {
-                SMStateDescription.Add(SMState_values[i], "");
-            }
-        }
+        // description des états
+        private Dictionary<SMState, string> SMStateDescription;
 
         public void setSMStateDescription(SMState state, string desciption)
         {
@@ -66,7 +49,68 @@ namespace ConsoleApp_StatMac
             return SMStateDescription[state];
         }
 
+        // dictionary nom variable et son type
+        public _ExtVarDef ExtVarDef;
+        // dictionary nom variable et sa valeur
+        public _ExtVarVal ExtVarVal;
+
+        // etats transitions de la machine
+        private Dictionary<SMState, Tuple<SMState, _ExtVarVal>> SMStatesTransitions;
+
+            
+
+        public StateMachine(_ExtVarDef? _ext_def)
+        {
+            SMStateDescription = new Dictionary<SMState, string>();
+            
+            var SMState_values = Enum.GetValues<SMState>();
+            for (int i = 0; i < SMState_values.Length; i++)
+            {
+                SMStateDescription.Add(SMState_values[i], "");
+            }
+            
+            ExtVarDef = new _ExtVarDef();
+            ExtVarVal = new _ExtVarVal();
+            // copy definition des valeurs du vecteur
+            if (_ext_def !=null)
+            {
+                ExtVarDef.Clear();
+                foreach (var k in _ext_def.Keys) 
+                {
+                    ExtVarDef.Add(k, _ext_def[k]);
+                }
+            }
+
+            // SMStatesTransitions initialise à des valeurs state suivant  =state_0 et vecteur transitions à valeurs 0, "" ou false
+            SMStatesTransitions = new Dictionary<SMState, Tuple<SMState, _ExtVarVal>>();
+
+            for (int j = 0; j < SMState_values.Length; j++)
+            {
+                // remplit avec liste de valeurs nulles les vecteurs de transisions
+                _ExtVarVal transitionVector = new _ExtVarVal();
+                foreach (var k in ExtVarDef.Keys)
+                {
+                    object? o = Activator.CreateInstance(ExtVarDef[k]);
+                    if (o is int) o = (int)0;
+                    else if(o is string) o = (string)"";
+                    else if (o is bool) o = (bool)false;
+                    else o= null;
+
+                    transitionVector.Add(k, o);
+                }
+                Tuple<SMState, _ExtVarVal> transitiondef = new Tuple<SMState, _ExtVarVal>(SMState.state_0, transitionVector);
+                SMStatesTransitions.Add(SMState_values[j],transitiondef);
+            }
+
+
+        }
+
         public void doWork()
+        {
+            int n = Enum.GetNames(typeof(SMState)).Length;
+        }
+
+        public void StateMachineWork(_ExtVarVal variablesvalues)
         {
             int n = Enum.GetNames(typeof(SMState)).Length;
         }
