0ca385debcf3293ec99c82c2d9c578c621356d84
pcreyssels <pcreyssels@free.fr>
2023-02-01 00:01:16 +0100

    A

--- WebAspTests/ConsoleApp_StatMac/Program.cs
+++ WebAspTests/ConsoleApp_StatMac/Program.cs
@@ -8,7 +8,7 @@ using System.Net.Http.Headers;
 using System.Reflection;
 using System.Reflection.Emit;
 
-using _smState = ConsoleApp_StatMac.StateMachine.SMState;
+using _smState = ConsoleApp_StatMac.StateMachine2.SMState;
 
 Console.WriteLine("Hello, World!");
 var i1 = new int[0].FirstOrDefault();
@@ -73,17 +73,101 @@ bool predresult = pred1(myobject);
 Predicate<ExpandoObject> preo = (ExpandoObject parametres) => ((dynamic)parametres).prop3 == "dded";
 // *********** fin Tests
 
+
+string external_variable = "bonjour";
+
+//  ************* statemachine1 *************
 StateMachine sma = new StateMachine();
 // définition des labels états
-sma.setSMStateDescription(StateMachine.SMState.state_1, "etat 0");
-sma.setSMStateDescription(StateMachine.SMState.state_1, "etat 1");
-sma.setSMStateDescription(StateMachine.SMState.state_2, "etat 2");
-sma.setSMStateDescription(StateMachine.SMState.state_3, "etat 3");
-sma.setSMStateDescription(StateMachine.SMState.state_4, "etat 4");
-sma.setSMStateDescription(StateMachine.SMState.state_5, "etat 5");
-sma.setSMStateDescription(StateMachine.SMState.state_6, "etat 6");
-sma.setSMStateDescription(StateMachine.SMState.state_7, "etat 7");
-sma.setSMStateDescription(StateMachine.SMState.state_8, "etat 8");
+sma.setSMStateDescription(0, "etat 0");
+sma.setSMStateDescription(1, "etat 1");
+sma.setSMStateDescription(2, "etat 2");
+sma.setSMStateDescription(3, "etat 3");
+sma.setSMStateDescription(4, "etat 4");
+sma.setSMStateDescription(5, "etat 5");
+
+// définition des tansitions
+/*
+ * 0 -> 1
+ * 0 -> 2
+ * 0 -> 3
+ * 1 -> 2
+ * 2 -> 3
+ * 3 -> 4
+ * 4 -> 5
+ */
+sma.StateMachineSetStatePair(0, 1);
+sma.StateMachineSetStatePair(0, 2);
+sma.StateMachineSetStatePair(0, 3);
+sma.StateMachineSetStatePair(1, 2);
+sma.StateMachineSetStatePair(2, 3);
+sma.StateMachineSetStatePair(3, 4);
+sma.StateMachineSetStatePair(4, 5);
+sma.StateMachineSetStatePair(4, 1);
+sma.StateMachineSetStatePair(5, 0);
+
+// définition des transitions
+// transition 0 -> 1
+ExpandoObject eo = new ExpandoObject();
+Predicate<ExpandoObject> predicate_1 = (ExpandoObject _eo) => {
+    bool retv = false;
+    if (external_variable == "bonjour")
+        retv = true;
+
+    return retv;
+};
+sma.StateMachine_setStateLine(0, 1, eo, predicate_1);
+
+predicate_1 = _ => true;
+sma.StateMachine_setStateLine(1, 2, eo, predicate_1);
+sma.StateMachine_setStateLine(2, 3, eo, predicate_1);
+
+dynamic eo1 = new ExpandoObject();
+eo1.prop1 = "valeur 1";
+Predicate<ExpandoObject> predicate_2 = (ExpandoObject _eo) => {
+    bool retv = false;
+    if (((dynamic)_eo).prop1 == "valeur 1")
+        retv = true;
+    return retv;
+};
+sma.StateMachine_setStateLine(3, 4, eo1, predicate_2);
+
+
+sma.StateMachineReset();
+int[] pr_st = sma.getPreviousStates(0);
+int curstat = 0;
+int nexsta = 0;
+int y = 0;
+for (int sts=0; sts<10;sts++)
+{
+    y = 0;
+    Tuple<int,int> d = new Tuple<int, int>(3,4);
+    if (y==1) 
+        ((dynamic)sma.StateMachineDescription[d].Item1).prop1 = "autre";
+    nexsta = sma.StateMachineWork();
+    Console.WriteLine($" etat : {nexsta.ToString()}");
+    curstat = sma.SMcurrentState;
+}
+curstat = sma.SMcurrentState;
+nexsta = sma.StateMachineWork();
+curstat = sma.SMcurrentState;
+
+
+
+
+
+// ************* statemachine2 *************
+StateMachine2 sma2 = new StateMachine2();
+// définition des labels états
+sma2.setSMStateDescription(StateMachine2.SMState.state_1, "etat 0");
+sma2.setSMStateDescription(StateMachine2.SMState.state_1, "etat 1");
+sma2.setSMStateDescription(StateMachine2.SMState.state_2, "etat 2");
+sma2.setSMStateDescription(StateMachine2.SMState.state_3, "etat 3");
+sma2.setSMStateDescription(StateMachine2.SMState.state_4, "etat 4");
+sma2.setSMStateDescription(StateMachine2.SMState.state_5, "etat 5");
+sma2.setSMStateDescription(StateMachine2.SMState.state_6, "etat 6");
+sma2.setSMStateDescription(StateMachine2.SMState.state_7, "etat 7");
+sma2.setSMStateDescription(StateMachine2.SMState.state_8, "etat 8");
 
 // définition des tansitions
 /*
@@ -100,25 +184,25 @@ sma.setSMStateDescription(StateMachine.SMState.state_8, "etat 8");
  * 8 -> 0
  */
 int r = 0;
-r= sma.StateMachineSetStatePair(StateMachine.SMState.state_0, StateMachine.SMState.state_1);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_0, StateMachine.SMState.state_2);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_0, StateMachine.SMState.state_3);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_1, StateMachine.SMState.state_2);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_2, StateMachine.SMState.state_3);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_3, StateMachine.SMState.state_4);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_4, StateMachine.SMState.state_5);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_5, StateMachine.SMState.state_6);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_6, StateMachine.SMState.state_0);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_7, StateMachine.SMState.state_0);
-r = sma.StateMachineSetStatePair(StateMachine.SMState.state_8, StateMachine.SMState.state_0);
+r= sma2.StateMachineSetStatePair(StateMachine2.SMState.state_0, StateMachine2.SMState.state_1);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_0, StateMachine2.SMState.state_2);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_0, StateMachine2.SMState.state_3);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_1, StateMachine2.SMState.state_2);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_2, StateMachine2.SMState.state_3);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_3, StateMachine2.SMState.state_4);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_4, StateMachine2.SMState.state_5);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_5, StateMachine2.SMState.state_6);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_6, StateMachine2.SMState.state_0);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_7, StateMachine2.SMState.state_0);
+r = sma2.StateMachineSetStatePair(StateMachine2.SMState.state_8, StateMachine2.SMState.state_0);
 
-string external_variable = "toto bonjour";
 
-_smState[] _sma = sma.getPreviousStates(StateMachine.SMState.state_0);
+
+_smState[] _sma = sma2.getPreviousStates(StateMachine2.SMState.state_0);
 
 // définition des transitions
 // transition 0 -> 1
-ExpandoObject eo= new ExpandoObject();
+eo= new ExpandoObject();
 Predicate<ExpandoObject> predicate11 = (ExpandoObject _eo) => {
     bool retv = false;
     if (external_variable == "bonjour")
@@ -126,21 +210,21 @@ Predicate<ExpandoObject> predicate11 = (ExpandoObject _eo) => {
     
     return retv;
 };
-sma.StateMachine_setStateLine(StateMachine.SMState.state_0, StateMachine.SMState.state_1, eo, predicate11);
+sma2.StateMachine_setStateLine(StateMachine2.SMState.state_0, StateMachine2.SMState.state_1, eo, predicate11);
 
 
 
 
-sma.StateMachineReset();
-sma.SMcurrentState = StateMachine.SMState.state_0;
+sma2.StateMachineReset();
+sma2.SMcurrentState = StateMachine2.SMState.state_0;
 
-sma.StateMachineWork();
-StateMachine.SMState cs = sma.SMcurrentState;
+sma2.StateMachineWork();
+StateMachine2.SMState cs = sma2.SMcurrentState;
 
 external_variable = "bonjour";
 
-sma.StateMachineWork();
-cs = sma.SMcurrentState;
+sma2.StateMachineWork();
+cs = sma2.SMcurrentState;
 
 
 
--- WebAspTests/ConsoleApp_StatMac/StateMachine.cs
+++ WebAspTests/ConsoleApp_StatMac/StateMachine.cs
@@ -10,9 +10,156 @@ using System.Threading.Tasks;
 namespace ConsoleApp_StatMac
 {
 
-
-
     internal class StateMachine
+    {
+        public Dictionary<int, string> SMStatesDescription;
+        
+
+        private int _SMcurrentState;
+        public int SMcurrentState
+        {
+            get => _SMcurrentState;
+            set
+            {
+                _SMcurrentState = value;
+            }
+        }
+
+        private int _foundTransitionsStates;
+        public int foundTransitionsStates
+        {
+            get => _foundTransitionsStates;
+        }
+
+        public void StateMachineReset()
+        {
+            _SMcurrentState = 0;
+        }
+       
+
+        public void setSMStateDescription(int state, string description)
+        {
+            if (SMStatesDescription.ContainsKey(state))
+                SMStatesDescription[state] = description;
+            else
+                SMStatesDescription.Add(state, description);
+        }
+
+        public string getSMStateDescription(int state)
+        {
+            return SMStatesDescription[state];
+        }
+
+        // dictionary nom variable et son type
+        // etat actif, etat suivant variables , predicat
+        public Dictionary<Tuple<int, int>, Tuple<ExpandoObject, Predicate<ExpandoObject>>> StateMachineDescription;
+        // dictionary nom variable et sa valeur
+
+
+        public StateMachine()
+        {
+            SMStatesDescription = new Dictionary<int, string>();
+            StateMachineDescription = new Dictionary<Tuple<int, int>, Tuple<ExpandoObject, Predicate<ExpandoObject>>>();
+
+            foreach (int k in SMStatesDescription.Keys)
+            {
+                // initialise avec des états
+                ExpandoObject eo = new ExpandoObject();
+                Predicate<ExpandoObject> pre = (ExpandoObject _eo) => false;
+                Tuple<ExpandoObject, Predicate<ExpandoObject>> transitiondef =
+                    new Tuple<ExpandoObject, Predicate<ExpandoObject>>(eo, pre);
+                Tuple<int, int> states = new Tuple<int, int>(k, k);
+                StateMachineDescription.Add(states, transitiondef);
+            }
+        }
+
+        public int StateMachineSetStatePair(int _currentState, int _nextState)
+        {
+            int retval = 0;
+            Predicate<ExpandoObject> predicate = (ExpandoObject _eo) => false;
+            ExpandoObject eo = new ExpandoObject();
+            Tuple<int, int> states = new Tuple<int, int>(_currentState, _nextState);
+            if (!StateMachineDescription.ContainsKey(states))
+            {
+                StateMachineDescription.Add(new Tuple<int, int>(_currentState, _nextState), new Tuple<ExpandoObject, Predicate<ExpandoObject>>(eo, predicate));
+            }
+            else
+                retval = -1; //couple clé déjà enregistrée
+
+
+            return retval;
+        }
+
+        public void StateMachine_setStateLine(int currentState, int nextState, ExpandoObject variables, Predicate<ExpandoObject> transitionFunc)
+        {
+            Tuple<int, int> states = new Tuple<int, int>(currentState, nextState);
+            Tuple<ExpandoObject, Predicate<ExpandoObject>> transitiondef = new Tuple<ExpandoObject, Predicate<ExpandoObject>>(variables, transitionFunc);
+            if (!StateMachineDescription.ContainsKey(states))
+            {
+                StateMachineDescription.Add(states, transitiondef);
+            }
+            else
+            {
+                StateMachineDescription[states] = transitiondef;
+            }
+
+        }
+
+        public int StateMachineWork()
+        {
+            List<int> list_nextstates = new List<int>();
+            int nextState = 0;
+            int morethan1transitionmatch = 0;
+            foreach (Tuple<int, int> k in StateMachineDescription.Keys)
+            {
+                // debug Console.WriteLine(k.Item1.ToString() + "   -   " + k.Item2.ToString());
+                if (k.Item1 == _SMcurrentState)
+                {
+
+                    Predicate<ExpandoObject> predicate = StateMachineDescription[k].Item2;
+                    bool transition = predicate(StateMachineDescription[k].Item1);
+                    if (transition)
+                    {
+                        nextState = k.Item2;
+                        morethan1transitionmatch += 1;
+                        // debug  Console.WriteLine(" match !");
+                    }
+                }
+            }
+            if (morethan1transitionmatch == 0)
+            {
+                // return _SMcurrentState;
+            }
+            else if (morethan1transitionmatch == 1)
+            {
+                _SMcurrentState = nextState;
+            }
+            else if (morethan1transitionmatch > 1)
+            {
+                // plus d'une transition possible erreur conception statemchine on avance pas
+                return _SMcurrentState;
+            }
+            _foundTransitionsStates = morethan1transitionmatch;
+            return _SMcurrentState;
+        }
+
+        public int[] getPreviousStates(int currentState)
+        {
+            List<int> list_prevstates = new List<int>();
+            foreach (Tuple<int, int> k in StateMachineDescription.Keys)
+            {
+                if ((k.Item2 == currentState) && !list_prevstates.Contains(k.Item1))
+                    list_prevstates.Add(k.Item1);
+            }
+
+            return list_prevstates.ToArray();
+        }
+
+        
+
+    }
+
+    internal class StateMachine2
     {
         public enum SMState : ushort
         {
@@ -68,7 +215,7 @@ namespace ConsoleApp_StatMac
 
 
 
-        public StateMachine()
+        public StateMachine2()
         {
             SMStateDescription = new Dictionary<SMState, string>();
             StateMachineDescription = new Dictionary<Tuple<SMState, SMState>, Tuple<ExpandoObject, Predicate<ExpandoObject>>>();
