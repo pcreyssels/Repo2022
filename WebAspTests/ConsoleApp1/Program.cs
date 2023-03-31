f10bcc1a418d11ceaf6e01091d2e539faada803e
pcreyssels <pcreyssels@free.fr>
2023-03-31 11:10:54 +0200

    A

--- WebAspTests/ConsoleApp1/Program.cs
+++ WebAspTests/ConsoleApp1/Program.cs
@@ -1,12 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlTypes;
-using System.Linq;
 using System.Reflection;
-using System.Runtime.Remoting;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 
 namespace ConsoleApp1
--- WebAspTests/ConsoleApp1/Properties/AssemblyInfo.cs
+++ WebAspTests/ConsoleApp1/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -32,5 +32,7 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
+
+[assembly: AssemblyInformationalVersion("0.0.2.0")]
