249d5272bd627cd91c88dfde50ed7519e7b90f62
pcreyssels <pcreyssels@free.fr>
2023-05-09 14:19:35 +0200

    a

---
+++ WpfAppNF48/SimpleMVVM/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using SimpleMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace SimpleMVVM
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            SimpleMVVM.View.Window1 window = new View.Window1();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+
+
+   
+
+}
