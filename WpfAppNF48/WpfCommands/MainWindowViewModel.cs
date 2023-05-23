2c0e9ebcaad2b8deeb8ea1840db3ab0f42dc7381
pcreyssels <pcreyssels@free.fr>
2023-05-23 18:59:46 +0200

    a

--- WpfAppNF48/WpfCommands/MainWindowViewModel.cs
+++ WpfAppNF48/WpfCommands/MainWindowViewModel.cs
@@ -10,116 +10,117 @@ using System.Xml.Linq;
 
 namespace WpfCommands
 {
-    internal class MainWindowViewModel : INotifyPropertyChanged
+internal class MainWindowViewModel : INotifyPropertyChanged
+{
+
+    private int _Propint=3;
+    private string _Propstring;
+    private double _Propdouble=5;
+
+    private User User1 { get; set; }
+
+    public int Propint
     {
-
-        private int _Propint=3;
-        private string _Propstring;
-        private double _Propdouble=5;
-
-        private User User1 { get; set; }
-
-        public int Propint
+        get
         {
-            get
-            {
-                return _Propint;
-            }
-            set
-            {
-                _Propint = value;
-                OnPropertyChanged("Propint");
-            }
+            return _Propint;
         }
-        public string Propstring
+        set
         {
-            get
-            {
-                return _Propstring;
-            }
-            set
-            {
-                _Propstring = value;
-                OnPropertyChanged("Propstring");
-            }
+            _Propint = value;
+            OnPropertyChanged("Propint");
         }
-        public double Propdouble
-        {
-            get
-            {
-                return _Propdouble;
-            }
-            set
-            {
-                _Propdouble = value;
-                OnPropertyChanged("Propdouble");
-            }
-        }
-
-        private ICommand m_ButtonCommand;
-        public ICommand ButtonCommand
-        {
-            get
-            {
-                return m_ButtonCommand;
-            }
-            set
-            {
-                m_ButtonCommand = value;
-            }
-        }
-        public MainWindowViewModel()
-        {
-            Predicate<object> _predicate = o => (o ==null ||(string) o == "heloi");
-            ButtonCommand = new RelayCommand(_predicate, new Action<object>(DoSomething));
-
-
-            // inscription aux événements du modèle
-            User1.PropertyChanged += Model_PropertyChanged;
-        }
-
-
-        private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            if (e.PropertyName == "Name")
-            {
-                DoSomething2();
-            }
-        }
-
-        public void DoSomething(object obj)
-        {
-            // do Something
-            int j = 12;
-            j = j + 1;
-            Propint = 32;
-
-            // modification du modèle par le viewmodel
-            User1.Name = "toto";
-        }
-
-        public void DoSomething2()
-        {
-            // do Something
-            int j = 12;
-            j = j + 1;
-            
-        }
-
-
-        #region INotifyChangeProperty  
-
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void OnPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
-
-        #endregion
-
     }
+    public string Propstring
+    {
+        get
+        {
+            return _Propstring;
+        }
+        set
+        {
+            _Propstring = value;
+            OnPropertyChanged("Propstring");
+        }
+    }
+    public double Propdouble
+    {
+        get
+        {
+            return _Propdouble;
+        }
+        set
+        {
+            _Propdouble = value;
+            OnPropertyChanged("Propdouble");
+        }
+    }
+
+    private ICommand m_ButtonCommand;
+    public ICommand ButtonCommand
+    {
+        get
+        {
+            return m_ButtonCommand;
+        }
+        set
+        {
+            m_ButtonCommand = value;
+        }
+    }
+    public MainWindowViewModel()
+    {
+        Predicate<object> _predicate = o => (o ==null ||(string) o == "heloi");
+        ButtonCommand = new RelayCommand(_predicate, new Action<object>(DoSomething));
+        User1 = new User();
+
+        // inscription aux événements du modèle
+        User1.PropertyChanged += Model_PropertyChanged;
+    }
+
+
+    private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName == "Name")
+        {
+            DoSomething2();
+        }
+    }
+
+    // appelée par commande
+    public void DoSomething(object obj)
+    {
+        // do Something
+        int j = 12;
+        j = j + 1;
+        //Propint = 32;
+
+        // modification du modèle par le viewmodel
+        User1.Name = "toto";
+    }
+
+    public void DoSomething2()
+    {
+        // do Something
+        int j = 12;
+        j = j + 1;
+        Propint = 100;
+    }
+
+
+    #region INotifyChangeProperty  
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    private void OnPropertyChanged(string propertyName)
+    {
+        if (PropertyChanged != null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    #endregion
+
+}
 }
 
--- WpfAppNF48/WpfCommands/Person.cs
+++ WpfAppNF48/WpfCommands/Person.cs
@@ -9,65 +9,65 @@ using System.Threading.Tasks;
 
 namespace WpfCommands
 {
-    internal class Person : INotifyPropertyChanged
-    {
+internal class Person : INotifyPropertyChanged
+{
 
-		/*
-		 * To implement INotifyPropertyChanged you need to declare the PropertyChanged event 
-		 * and create the OnPropertyChanged method. 
-		 * Then for each property you want change notifications for, 
-		 * you call OnPropertyChanged whenever the property is updated.
-		 */
-		private int age;
+	/*
+		* To implement INotifyPropertyChanged you need to declare the PropertyChanged event 
+		* and create the OnPropertyChanged method. 
+		* Then for each property you want change notifications for, 
+		* you call OnPropertyChanged whenever the property is updated.
+		*/
+	private int age;
 
-		public int  Age
-		{
-			get { return age; }
-			set 
-			{ 
-				age = value;
-                // Call OnPropertyChanged whenever the property is updated
-                OnPropertyChanged();
-            }
-		}
-
-
-		private string firstName;
-
-		public string FirstName
-        {
-			get { return firstName; }
-			set 
-            { 
-                firstName = value;
-                // Call OnPropertyChanged whenever the property is updated
-                OnPropertyChanged();
-            }
-		}
-
-		private string lastName;
-
-		public string LastName
-        {
-			get { return lastName; }
-			set 
-			{ 
-				lastName = value;
-                // Call OnPropertyChanged whenever the property is updated
-                OnPropertyChanged();
-            }
-		}
-
-        // Declare the event
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        // Create the OnPropertyChanged method to raise the event
-        // The calling member's name will be used as the parameter.
-        protected void OnPropertyChanged([CallerMemberName] string name = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+	public int  Age
+	{
+		get { return age; }
+		set 
+		{ 
+			age = value;
+            // Call OnPropertyChanged whenever the property is updated
+            OnPropertyChanged();
         }
+	}
+
+
+	private string firstName;
+
+	public string FirstName
+    {
+		get { return firstName; }
+		set 
+        { 
+            firstName = value;
+            // Call OnPropertyChanged whenever the property is updated
+            OnPropertyChanged();
+        }
+	}
+
+	private string lastName;
+
+	public string LastName
+    {
+		get { return lastName; }
+		set 
+		{ 
+			lastName = value;
+            // Call OnPropertyChanged whenever the property is updated
+            OnPropertyChanged();
+        }
+	}
+
+    // Declare the event
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    // Create the OnPropertyChanged method to raise the event
+    // The calling member's name will be used as the parameter.
+    protected void OnPropertyChanged([CallerMemberName] string name = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
     }
+}
 
 
     public class User : ObservableObject
