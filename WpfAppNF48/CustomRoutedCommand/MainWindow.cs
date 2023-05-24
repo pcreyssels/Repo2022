09ee2ab4bae1a52dcdb91248987424e6e157f9d6
pcreyssels <pcreyssels@free.fr>
2023-05-24 11:26:46 +0200

    a

--- WpfAppNF48/CustomRoutedCommand/MainWindow.cs
+++ WpfAppNF48/CustomRoutedCommand/MainWindow.cs
@@ -1,6 +1,7 @@
 ﻿// // Copyright (c) Microsoft. All rights reserved.
 // // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -18,11 +19,18 @@ namespace CustomRoutedCommand
         public MainWindow()
         {
             InitializeComponent();
+            Debug.WriteLine("initialize mainwindows");
         }
 
         // ExecutedRoutedEventHandler for the custom color command.
         private void ColorCmdExecuted(object sender, ExecutedRoutedEventArgs e)
         {
+            object so = e.Source;
+            object oso = e.OriginalSource;
+            object se = e.Source;
+            Debug.WriteLine($" executed source {so.GetType().Name} -> name : {((FrameworkElement)so).Name}");
+            Debug.WriteLine($"  sender {se.GetType().Name} -> name : {((FrameworkElement)se).Name}");
+            Debug.WriteLine($"  original {oso.GetType().Name} -> name : {((FrameworkElement)oso).Name}");
             var target = e.Source as Panel;
             if (target != null)
             {
@@ -33,6 +41,13 @@ namespace CustomRoutedCommand
         // CanExecuteRoutedEventHandler for the custom color command.
         private void ColorCmdCanExecute(object sender, CanExecuteRoutedEventArgs e)
         {
+            object so = e.Source;
+            object se = e.Source;
+            object oso = e.OriginalSource;
+            Debug.WriteLine($" canexec {so.GetType().Name} -> name : {((FrameworkElement)so).Name}");
+            Debug.WriteLine($"  sender {se.GetType().Name} -> name : {((FrameworkElement)se).Name}");
+            Debug.WriteLine($"  original {oso.GetType().Name} -> name : {((FrameworkElement)oso).Name}");
+
             if (e.Source is Panel)
             {
                 e.CanExecute = true;
