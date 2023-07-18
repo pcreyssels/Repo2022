c5fd1359fc61ee886c9c9086caedcb50c0212bca
pcreyssels <pcreyssels@free.fr>
2023-07-18 14:20:05 +0200

    a

--- WpfAppNF48/InputValidation/MainWindow.xaml.cs
+++ WpfAppNF48/InputValidation/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace InputValidation
 {
@@ -20,9 +23,42 @@ namespace InputValidation
     /// </summary>
     public partial class MainWindow : Window
     {
+
+
+        public static RoutedCommand Command1 { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            Command1 = new RoutedCommand();
+        }
+
+
+        public void Command1Executed(object sender, ExecutedRoutedEventArgs e)
+        {
+
+            string p = (string)e.Parameter;
+            
+        }
+
+        public void Command1CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            if (datePicker.SelectedDate != null) 
+            {
+                int i = ErrorDisplay.Items.Count;
+                if (i == 0)
+                    e.CanExecute = true;
+                else
+                    e.CanExecute = false;
+            }
+            else
+                e.CanExecute = false;
+
         }
     }
+
+
+    
+
 }
