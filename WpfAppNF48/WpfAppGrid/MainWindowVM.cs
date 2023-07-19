2bfd8c2a049b47996a0e2a3b2222622bb2041a03
pcreyssels <pcreyssels@free.fr>
2023-07-19 17:07:10 +0200

    a

---
+++ WpfAppNF48/WpfAppGrid/MainWindowVM.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace WpfAppGrid
+{
+    internal class MainWindowVM :INotifyPropertyChanged
+    {
+
+        internal MainWindowVM() 
+        {
+            DataGridDatas = new DataTable();
+            DataGridDatas.Columns.Add("Nom", typeof(string));
+            DataGridDatas.Columns.Add("Age", typeof(int));
+            DataRow dr = DataGridDatas.NewRow();
+            dr["Nom"] = "DUPONT";
+            dr["Age"] = 23;
+            DataGridDatas.Rows.Add(dr);
+            dr = DataGridDatas.NewRow();
+            dr["Nom"] = "DURAND";
+            dr["Age"] = 40;
+            DataGridDatas.Rows.Add(dr);
+        }  
+
+        private DataTable dataGridDatas;
+        public DataTable DataGridDatas
+        {
+            get { return dataGridDatas; }
+            set
+            {
+                dataGridDatas = value;
+                OnPropertyChanged("DataGridDatas");
+            }
+        }
+
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string nomPte)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nomPte));
+        }
+        #endregion
+    }
+}
