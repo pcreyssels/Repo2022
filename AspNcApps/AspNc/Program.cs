f8f4e537b0d778d1375bd2527fc0d4bc1f342e0d
pcreyssels <pcreyssels@free.fr>
2024-11-13 22:44:51 +0100

    a

--- AspNcApps/AspNc/Program.cs
+++ AspNcApps/AspNc/Program.cs
@@ -23,3 +23,7 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+//https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/iis/?view=aspnetcore-8.0#iis-configuration
+//https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/directory-structure?view=aspnetcore-8.0
+//https://learn.microsoft.com/en-us/answers/questions/1321605/host-name-in-iis-site-bindings-purpose-and-usage
